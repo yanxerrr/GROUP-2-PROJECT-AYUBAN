e5484e2fca3cec8523172c4b2ccca3828bbd7580
yanxerrr <ggxfire22@gmail.com>
2025-05-09 14:35:19 +0800

    Update LibLogin.java

--- src/LibLogin.java
+++ src/LibLogin.java
@@ -155,8 +155,6 @@ public class LibLogin extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-
-        
         String username = txtUser.getText();
         String password = new String(txtPass.getPassword());
 
